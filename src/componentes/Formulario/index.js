4a827e91b727022f8eebc9f8c19c2f69e6bf8522
vinips1993 <rm550907@fiap.com.br>
2024-01-25 08:29:49 -0300

    Lista de nomes criada

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className='formulario'>
             <div>
@@ -10,6 +22,7 @@ const Formulario = () => {
                     <CampoTexto label="Nome" placeholder="Digite seu Nome" />
                     <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
                     <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem" />
+                    <ListaSuspensa label="Time" itens={times}/>
                 </form>
             </div>
         </section>
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,7 +1,18 @@
 import './ListaSuspensa.css'
 
-const ListaSuspensa = () => {
-    
+const ListaSuspensa = (props) => {
+    console.log(props.item)
+
+    return (
+        <div className='lista-supensa'>
+            <label>{props.label}</label>
+            <select>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select> 
+        </div>
+    )
 }
 
 export default ListaSuspensa
